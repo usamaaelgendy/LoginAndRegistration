d4e78d2b49309a9c7635eb1f8cc777ac0669514d
usamaElgendy <usamaaelgendy@gmail.com>
2021-01-17 10:24:51 +0200

    fixed error

--- app/src/main/java/com/example/loginandregistration/HomeActivity.java
+++ app/src/main/java/com/example/loginandregistration/HomeActivity.java
@@ -1,7 +1,5 @@
 package com.example.loginandregistration;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,6 +10,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.snackbar.Snackbar;
 
